0b53cf7706f63ccd8d682ec902c169b87a765b84
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-25 10:14:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "hello"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
